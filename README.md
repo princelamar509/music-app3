a1f464210aafb657b818d65193a474fd2c376b87
JAMES M <168496401+princelamar509@users.noreply.github.com>
2024-09-12 23:15:10 -0400

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-this is a music app create bu using react sass for styling
--- music-app3/vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/music-app/', // replace with your repository name
-});
-
