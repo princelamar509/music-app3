5da82d61d4d9a49228efcd105f94e8166b9a85f1
JAMES M <168496401+princelamar509@users.noreply.github.com>
2024-09-12 23:12:43 -0400

    pink

--- music-app3/src/components/Navb.jsx
+++ music-app3/src/components/Navb.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMusic } from '@fortawesome/free-solid-svg-icons'; // Import a valid icon
 
 const Nav = ({ setLibraryStatus, libraryStatus }) => {
     return (
         <nav>
-            <h1>Harmony Hub</h1>
+            <h1>
+                Harmony Hub 
+                <FontAwesomeIcon icon={faMusic} /> {/* Use the imported icon */}
+            </h1>
             <button
                 onClick={() => {
                     setLibraryStatus(!libraryStatus);
-                }}fcdddc 
+                }}
             >
                 <h4>Library</h4>
             </button>
@@ -15,4 +20,4 @@ const Nav = ({ setLibraryStatus, libraryStatus }) => {
     );
 };
 
-export default Nav; 
+export default Nav;
